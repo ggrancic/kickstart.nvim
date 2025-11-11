1f907df1d8129e447a1df1db04d7e7401dd57382
Gianluca GL <grancicg@gmail.com>
2025-11-11 20:09:22 -0300

    ensure_installed enabled on lsp and treesitter

--- lua/kickstart/plugins/kanagawa.lua
+++ lua/kickstart/plugins/kanagawa.lua
@@ -2,5 +2,8 @@ return {
   'rebelot/kanagawa.nvim',
   opts = {
     transparent = false,
+    commentStyle = { italic = true },
+    keywordStyle = { italic = true },
+    statementStyle = { bold = true },
   },
 }
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -253,6 +253,10 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'ts_ls', -- JS/TS
+        'emmet_language_server', -- Emmet
+        'html', -- HTML
+        'cssls', -- CSS
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
